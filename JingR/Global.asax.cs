21832a07dc0285ad7f5515eefc437dbf5d70c0c3
Evan Dontje <e.dontje@techsmith.com>
2012-04-18 08:45:34 -0400

    Updated gitignore to ignore resharper and cleaned up gloabl.asax

--- JingR/Global.asax.cs
+++ JingR/Global.asax.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace JingR
 {
-   public class Global : System.Web.HttpApplication
+   public class Global : HttpApplication
    {
 
       void Application_Start( object sender, EventArgs e )
