1c671ab8f773e166be21c2139834c0ed99696dee
Evan Dontje <e.dontje@techsmith.com>
2012-04-18 09:26:17 -0400

    Cleaned up default.aspx

--- JingR/default.aspx
+++ JingR/default.aspx
@@ -2,4 +2,7 @@
 <asp:Content ID="head" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="body" ContentPlaceHolderID="Body" runat="server">
+   <p>Content</p>
+</asp:Content>
+<asp:Content ID="scripts" ContentPlaceHolderID="Scripts" runat="server">
 </asp:Content>
--- JingR/default.aspx.cs
+++ JingR/default.aspx.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace JingR
 {
-   public partial class _default : System.Web.UI.Page
+   public partial class Default : Page
    {
       protected void Page_Load( object sender, EventArgs e )
       {
-
       }
    }
 }
