29b89f067d9844c7cb91e8132c582ed34cc81ddb
Evan Dontje <e.dontje@techsmith.com>
2012-05-04 08:56:59 -0400

    Adding forgotten files from last checkin

---
+++ JingR/Modules/JingRModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace JingR.Modules
+{
+   public class JingRModule : NancyModule
+   {
+      public JingRModule()
+         : base( "/api" )
+      {
+         Get["{id}/foo"] = parameters =>
+                       {
+                          return "id: " + parameters.id;
+                       };
+      }
+   }
+}
