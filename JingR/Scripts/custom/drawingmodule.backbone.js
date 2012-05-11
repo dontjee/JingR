fed09b42efb68ec2e50d07711769e763361e6156
Evan Dontje <e.dontje@techsmith.com>
2012-05-11 09:26:38 -0400

    Added textbox support

--- JingR/Scripts/custom/drawingmodule.backbone.js
+++ JingR/Scripts/custom/drawingmodule.backbone.js
@@ -31,6 +31,70 @@
       }
    });
 
+   var Textbox = Backbone.Model.extend({
+      defaults: {
+         begin: { x: 0, y: 0 },
+         end: { x: 0, y: 0 }
+      }
+   });
+
+   var TextboxCollection = Backbone.Collection.extend({
+      model: Textbox
+   });
+
+   var TextboxView = Backbone.View.extend({
+      initialize: function () {
+         _.bindAll(this, 'render', 'modelChange'); // every function that uses 'this' as the current object should be in here
+
+         this.model.on('change:end', this.modelChange);
+      },
+      modelChange: function () {
+         this.textbox.remove();
+         this.render();
+      },
+      render: function () {
+         this.textbox = $('<textarea>');
+
+         var parentElement = $(this.options.parentElement);
+
+         parentElement.append(this.textbox);
+         this.textbox.css('position', 'absolute');
+
+         var x1 = this.model.attributes.begin.x,
+             x2 = this.model.attributes.end.x,
+             y1 = this.model.attributes.begin.y,
+             y2 = this.model.attributes.end.y;
+
+         var top = y1 > y2 ? y2 : y1;
+         var left = x1 > x2 ? x2 : x1;
+         var paperOffset = parentElement.offset();
+
+         this.textbox.css('left', left + paperOffset.left);
+         this.textbox.css('top', top + paperOffset.top);
+
+         var height = y2 > y1 ? y2 - y1 : y1 - y2;
+         height = height > 30 ? height : 30;
+         this.textbox.css('height', height);
+
+         var width = x2 > x1 ? x2 - x1 : x1 - x2;
+         width = width > 60 ? width : 60;
+         this.textbox.css('width', width);
+
+         return this; // for chainable calls, like .render().el
+      }
+   });
+
+   var DrawingButtonsView = Backbone.View.extend({
+      el: $('#drawingType'),
+      initialize: function () {
+         _.bindAll(this, 'getSelectedType');
+      },
+      getSelectedType: function () {
+         var selected = $(this.el).children('button.active');
+         return selected.data('type');
+      }
+   });
+
    var PaperView = Backbone.View.extend({
       el: $('#paper'),
       events: {
@@ -39,13 +103,18 @@
          'mouseup': 'mouseUp'
       },
       initialize: function () {
-         _.bindAll(this, 'render', 'mouseDown', 'mouseMove', 'mouseUp', 'setEndpointOfDrawing', 'addArrow'); // every function that uses 'this' as the current object should be in here
+         _.bindAll(this, 'render', 'mouseDown', 'mouseMove', 'mouseUp', 'setEndpointOfDrawing', 'addArrow', 'addTextbox'); // every function that uses 'this' as the current object should be in here
 
          this.arrows = new ArrowCollection();
          this.arrows.bind('add', this.addArrow); // collection event binder
 
+         this.textboxes = new TextboxCollection();
+         this.textboxes.bind('add', this.addTextbox);
+
          this.paper = Raphael("paper", 500, 500);
          this.paperOffset = $(this.el).offset();
+
+         this.drawingButtons = new DrawingButtonsView();
 
          this.render();
       },
@@ -55,6 +124,10 @@
          var self = this;
          _(this.arrows.models).each(function (arrow) {
             self.addArrow(arrow);
+         }, this);
+
+         _(this.textboxes.models).each(function (textbox) {
+            self.addTextbox(textbox);
          }, this);
       },
       mouseDown: function (e) {
@@ -70,13 +143,25 @@
             x: e.clientX - this.paperOffset.left,
             y: e.clientY - this.paperOffset.top
          };
-         this.drawing = new Arrow();
-         this.drawing.set({
-            begin: begin,
-            end: begin
-         });
 
-         this.arrows.add(this.drawing);
+         if (this.drawingButtons.getSelectedType() == 'arrow') {
+            this.drawing = new Arrow();
+            this.drawing.set({
+               begin: begin,
+               end: begin
+            });
+
+            this.arrows.add(this.drawing);
+         } else {
+            this.drawing = new Textbox();
+            this.drawing.set({
+               begin: begin,
+               end: begin
+            });
+
+            this.textboxes.add(this.drawing);
+         }
+
       },
       mouseMove: function (e) {
          if (!this.isDragging) {
@@ -110,6 +195,15 @@
             paper: self.paper
          });
          return arrowView.render();
+      },
+      addTextbox: function (textbox) {
+         var self = this;
+         var textboxView = new TextboxView({
+            model: textbox,
+            parentElement: self.el
+         });
+
+         return textboxView.render();
       }
    });
 
